de47c1f7326b639eb2f2f713d030a5367db9c1f9
Mustafa Tuncer <tuncerm@gmail.com>
2020-09-26 19:02:44 +0200

    Updated Spinner to Work as non overlay.

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Spinner.css';
 
-export default function Spinner(){
+export default function Spinner(props){
     return(
-        <div className="spinner-overlay">
+        <div className={`${props.asOverlay && 'spinner-overlay'}`}>
             <div className="spinner-main">
                 <div className="bounce3"></div>
                 <div className="bounce1"></div>
