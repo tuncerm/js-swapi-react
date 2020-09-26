ddbfee2ffa366febbd2c5914ff2e005fa88e00c3
Mustafa Tuncer <tuncerm@gmail.com>
2020-09-26 02:35:54 +0200

    Added a custom fetch hook.

---
+++ src/hooks/http.hook.js
@@ -0,0 +1,44 @@
+import {useState, useCallback, useRef, useEffect} from 'react';
+
+export const useFetch = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const fetchRequests = useRef([]);
+
+    const sendRequest = useCallback(async (url, method = 'GET', headers = {}, body = null ) => {
+        try{
+            setIsLoading(true); 
+            const abortCtrl = new AbortController();
+            fetchRequests.current.push(abortCtrl);
+            const response = await fetch(url, {
+                method,
+                body,
+                headers,
+                signal: abortCtrl.signal
+            });
+            const responseData = await response.json();
+            fetchRequests.current = fetchRequests.current.filter(reqCtrl => reqCtrl !== abortCtrl);
+            if(!response.ok){
+              throw new Error(responseData.message);
+            }
+            setIsLoading(false); 
+            return responseData;
+        } catch (err){
+            setError(err.message);
+            setIsLoading(false); 
+            throw err;
+        }
+    }, []);
+
+    const clearError = () => {
+        setError(null);
+    }
+
+    useEffect(()=> {
+        return () => {
+            fetchRequests.current.forEach(abortCtrl=>abortCtrl.abort());
+        };
+    }, []);
+
+    return {isLoading, error, sendRequest, clearError};
+}
