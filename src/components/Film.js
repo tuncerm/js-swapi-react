34849cfc0510def758bde314a851aa10cea184a6
Mustafa Tuncer <tuncerm@gmail.com>
2020-09-26 02:39:21 +0200

    Updated Film component to display data.

--- src/components/Film.js
+++ src/components/Film.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './Film.css';
 
-function Film() {
+function Film({movie}) {
+  const {title, opening_crawl, release_date} = movie;
+  const parts = opening_crawl.split('\r\n');
+
   return (
-    <div></div>
+    <div className="film-main">
+      <h2 className="film-title">{title}</h2>
+      <hr/>
+      <div className="film-detail-div">{opening_crawl}</div>
+      <hr/>
+      <p className="film-date">Release Date: {release_date}</p>
+    </div>
   );
 }
 
