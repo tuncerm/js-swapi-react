e5a1c5f3d7c19e691eb65db65cecf628d140eeaa
Mustafa Tuncer <tuncerm@gmail.com>
2020-09-27 01:43:42 +0200

    Step 3: People search page

--- src/components/Actor.js
+++ src/components/Actor.js
@@ -3,12 +3,16 @@ import React, { useState, useEffect, useContext } from 'react';
 import { CacheContext } from '../context/cache-context';
 
 import Spinner from './Spinner';
+import Modal from './Modal';
 
 import './Actor.css';
 
 export default function Actor({ actor }) {
     const {name, height, birth_year, gender, films} = actor;
     const [isLoading, setLoading] = useState(false);
+    const [modal, setModal] = useState(false);
+    const [modalUrl, setModalUrl] = useState("");
+
     const [actorFilms, setActorFilms] = useState([]);
 
     const { getData } = useContext(CacheContext);
@@ -33,17 +37,32 @@ export default function Actor({ actor }) {
         fetchFilmData();
     }, [films, getData]);
 
+    const openModal = (url) => {
+        setModalUrl(url);
+        setModal(true);
+    };
+    const closeModal = () => {
+        setModal(false);
+        setModalUrl("");
+    }
+
     return (
-        <div className={"actor-main"}>
-            {isLoading && <Spinner/>}
-            <h2 className="actor-title">{name}</h2>
-            <hr/>
-            <div className="actor-detail-div">
-                <p>Height: {height}</p>
-                <p>Born in : {birth_year}</p>
-                <p>{gender}</p>
-                {actorFilms.map(film=><p key={btoa(film.url)}>{film.title}</p>)}
+        <>
+            {modal && <Modal url={modalUrl} close={closeModal} />}
+            <div className={"actor-main"}>
+                {isLoading ? <Spinner/> : <div className="actor-detail-div">
+                    <div className="actor-detail-part">
+                        <h5 className="actor-title">{name}</h5>
+                        <p>Born in  {birth_year}</p>
+                        <p>Height {height}</p>
+                        <p>{gender}</p>
+                    </div>
+                    <div className="actor-detail-part">
+                        <h5 className="actor-title">Movies</h5>
+                        {actorFilms.map(film=><p key={btoa(film.url)} onClick={()=>{openModal(film.url)}} className="actor-movie-item">{film.title}</p>)}
+                    </div>
+                </div>}
             </div>
-        </div>
+        </>
     )
 }
