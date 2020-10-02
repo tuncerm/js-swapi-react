1d466f6e23c122420c137c22c57714e0f9f3d892
Mustafa Tuncer <tuncerm@gmail.com>
2020-10-03 00:43:39 +0200

    FilmCard Visual Improvements

--- src/components/FilmCard.js
+++ src/components/FilmCard.js
@@ -6,7 +6,7 @@ import './FilmCard.css';
 export default function FilmCard({movie}) {
   return (
     <div className="filmcard-main">
-      <Link to={`/films/${btoa(movie.url)}`}><h2 className="filmcard-title">{movie.title} E.{movie.episode_id}</h2></Link>
+      <Link to={`/films/${btoa(movie.url)}`} className="filmcard-title-link"><h2 className="filmcard-title">{movie.title} E.{movie.episode_id}</h2></Link>
       <hr/>
       <div className="filmcard-detail-div"><p>{movie.opening_crawl.split('\n\r').splice(0, 1)[0]}</p></div>
       <hr/>
