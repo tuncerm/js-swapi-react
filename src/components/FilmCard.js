aef08db5cafda570a030a01485f18e47ce10c7ae
Mustafa Tuncer <tuncerm@gmail.com>
2020-10-02 23:40:24 +0200

    Css changes for FilmCard

--- src/components/FilmCard.js
+++ src/components/FilmCard.js
@@ -5,12 +5,12 @@ import './FilmCard.css';
 
 export default function FilmCard({movie}) {
   return (
-    <div className="film-main">
-      <Link to={`/films/${btoa(movie.url)}`}><h2 className="film-title">{movie.title} E.{movie.episode_id}</h2></Link>
+    <div className="filmcard-main">
+      <Link to={`/films/${btoa(movie.url)}`}><h2 className="filmcard-title">{movie.title} E.{movie.episode_id}</h2></Link>
       <hr/>
-      <div className="film-detail-div"><p>{movie.opening_crawl.split('\n\r').splice(0, 1)[0]}</p></div>
+      <div className="filmcard-detail-div"><p>{movie.opening_crawl.split('\n\r').splice(0, 1)[0]}</p></div>
       <hr/>
-      <p className="film-date">Release Date: {movie.release_date}</p>
+      <p className="filmcard-date">Release Date: {movie.release_date}</p>
     </div>
   );
 }
