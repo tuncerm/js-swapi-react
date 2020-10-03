1f0b8bb8ca79221c68830142ca033acd5f862d17
Mustafa Tuncer <tuncerm@gmail.com>
2020-10-03 22:35:23 +0200

    SpeciesCard now accepts an url.

--- src/components/SpeciesCard.js
+++ src/components/SpeciesCard.js
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import {Link} from 'react-router-dom';
+
+import Spinner from './Spinner';
+
+import { CacheContext } from '../context/cache-context';
 
 import './SpeciesCard.css';
 
-export default function SpeciesCard({specie}) {
+export default function SpeciesCard({specie, url}) {
+  const [data, setData] = useState(specie);
+  const [isLoading, setLoading] = useState(false);
+  const { getData } = useContext(CacheContext);
+
+  useEffect(() => {
+    const fetchSpecieData = async () => {
+        try {
+            setLoading(true);
+            if(url){
+                const response = await getData(url);
+                setData(response);
+            }
+            setLoading(false);
+        } catch (e) {
+            console.log(e);
+        }
+    };
+    fetchSpecieData();
+}, [url, getData]);
+
+
   return (
     <div className="speciescard-main">
-      <Link to={`/species/${btoa(specie.url)}`} className="speciescard-title-link"><h2 className="speciescard-title">{specie.name}</h2></Link>
-      <hr/>
-      <div className="speciescard-detail-div">
-        <p>Language: {specie.language}</p>
-        <p>Designation: {specie.designation}</p>
-        <p>Classification: {specie.classification}</p>
-      </div>
+      {isLoading && <Spinner />}
+      {!isLoading && data && <>
+        <Link to={`/species/${btoa(data.url)}`} className="speciescard-title-link"><h2 className="speciescard-title">{data.name}</h2></Link>
+        <hr/>
+        <div className="speciescard-detail-div">
+          <p>Language: {data.language}</p>
+          <p>Designation: {data.designation}</p>
+          <p>Classification: {data.classification}</p>
+        </div>
+      </>}
     </div>
   );
 }
