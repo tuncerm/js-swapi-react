60956703f8313c3a524a55f8e499895eb5fee865
Mustafa Tuncer <tuncerm@gmail.com>
2020-10-03 22:35:06 +0200

    FilmCard now accepts an url.

--- src/components/FilmCard.js
+++ src/components/FilmCard.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import {Link} from 'react-router-dom';
+
+import Spinner from './Spinner';
+
+import { CacheContext } from '../context/cache-context';
 
 import './FilmCard.css';
 
-export default function FilmCard({movie}) {
+export default function FilmCard({movie, url}) {
+  const [data, setData] = useState(movie);
+  const [isLoading, setLoading] = useState(false);
+  const { getData } = useContext(CacheContext);
+
+  useEffect(() => {
+    const fetchFilmData = async () => {
+        try {
+            setLoading(true);
+            if(url){
+                const response = await getData(url);
+                setData(response);
+            }
+            setLoading(false);
+        } catch (e) {
+            console.log(e);
+        }
+    };
+    fetchFilmData();
+}, [url, getData]);
+
   return (
     <div className="filmcard-main">
-      <Link to={`/films/${btoa(movie.url)}`} className="filmcard-title-link"><h2 className="filmcard-title">{movie.title} E.{movie.episode_id}</h2></Link>
-      <hr/>
-      <div className="filmcard-detail-div"><p>{movie.opening_crawl.split('\n\r').splice(0, 1)[0]}</p></div>
-      <hr/>
-      <p className="filmcard-date">Release Date: {movie.release_date}</p>
+      {isLoading && <Spinner />}
+      {!isLoading && data && <>
+        <Link to={`/films/${btoa(data.url)}`} className="filmcard-title-link"><h2 className="filmcard-title">{data.title} E.{data.episode_id}</h2></Link>
+        <hr/>
+        <div className="filmcard-detail-div"><p>{data.opening_crawl.split('\n\r').splice(0, 1)[0]}</p></div>
+        <hr/>
+        <p className="filmcard-date">Release Date: {data.release_date}</p>
+      </>}
     </div>
   );
 }
