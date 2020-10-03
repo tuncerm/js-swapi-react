0aac4bbfb1581b10a0d11d9a626500f099ed4b7e
Mustafa Tuncer <tuncerm@gmail.com>
2020-10-03 22:58:13 +0200

    Update PlanetCard to accept urp as prop.

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -1,18 +1,45 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom';
+
+import Spinner from './Spinner';
+
+import { CacheContext } from '../context/cache-context';
 
 import './PlanetCard.css';
 
-export default function PlanetCard({planet}) {
+export default function PlanetCard({ planet, url }) {
+  const [data, setData] = useState(planet);
+  const [isLoading, setLoading] = useState(false);
+  const { getData } = useContext(CacheContext);
+
+  useEffect(() => {
+    const fetchPlanetData = async () => {
+      try {
+        setLoading(true);
+        if (url) {
+          const response = await getData(url);
+          setData(response);
+        }
+        setLoading(false);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchPlanetData();
+  }, [url, getData]);
+
   return (
     <div className="planetcard-main">
-      <Link to={`/planets/${btoa(planet.url)}`} className="planetcard-title-link"><h2 className="planetcard-title">{planet.name}</h2></Link>
-      <hr/>
-      <div className="filmcard-detail-div">
-        <p>Climate: {planet.climate}</p>
-        <p>Terrain: {planet.terrain}</p>
-        <p>Population: {planet.population}</p>
-      </div>
+      {isLoading && <Spinner />}
+      {!isLoading && data && <>
+        <Link to={`/planets/${btoa(data.url)}`} className="planetcard-title-link"><h2 className="planetcard-title">{data.name}</h2></Link>
+        <hr />
+        <div className="filmcard-detail-div">
+          <p>Climate: {data.climate}</p>
+          <p>Terrain: {data.terrain}</p>
+          <p>Population: {data.population}</p>
+        </div>
+      </>}
     </div>
   );
 }
