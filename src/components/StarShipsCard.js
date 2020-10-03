3b1050ccb2ed20d534074d5c2f447110ed594484
Mustafa Tuncer <tuncerm@gmail.com>
2020-10-03 22:58:35 +0200

    Update StarShipsCard to accept urp as prop.

--- src/components/StarShipsCard.js
+++ src/components/StarShipsCard.js
@@ -1,18 +1,52 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
 
-import './StarShipsCard.css';
+import Spinner from "./Spinner";
 
-export default function StarShipsCard({starship}) {
+import { CacheContext } from "../context/cache-context";
+
+import "./StarShipsCard.css";
+
+export default function StarShipsCard({ starship, url }) {
+  const [data, setData] = useState(starship);
+  const [isLoading, setLoading] = useState(false);
+  const { getData } = useContext(CacheContext);
+
+  useEffect(() => {
+    const fetchStarShipData = async () => {
+      try {
+        setLoading(true);
+        if (url) {
+          const response = await getData(url);
+          setData(response);
+        }
+        setLoading(false);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchStarShipData();
+  }, [url, getData]);
+
   return (
     <div className="starshipscard-main">
-      <Link to={`/starships/${btoa(starship.url)}`} className="starshipscard-title-link"><h2 className="starshipscard-title">{starship.name}</h2></Link>
-      <hr/>
-      <div className="starshipscard-detail-div">
-        <p>Model: {starship.model}</p>
-        <p>Manufacturer: {starship.manufacturer}</p>
-        <p>Cost: {starship.cost_in_credits}</p>
-      </div>
+      {isLoading && <Spinner />}
+      {!isLoading && data && (
+        <>
+          <Link
+            to={`/starships/${btoa(data.url)}`}
+            className="starshipscard-title-link"
+          >
+            <h2 className="starshipscard-title">{data.name}</h2>
+          </Link>
+          <hr />
+          <div className="starshipscard-detail-div">
+            <p>Model: {data.model}</p>
+            <p>Manufacturer: {data.manufacturer}</p>
+            <p>Cost: {data.cost_in_credits}</p>
+          </div>
+        </>
+      )}
     </div>
   );
 }
