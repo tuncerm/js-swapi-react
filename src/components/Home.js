5f1a11c6d9669dbfb25e6b672c33875596ad7882
Mustafa Tuncer <tuncerm@gmail.com>
2020-10-04 01:22:58 +0200

    Updated home component to host Searc functionality

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
+
+import Actor from "./Actor";
+
+import { useFetch } from "../hooks/http.hook";
+
 import "./Home.css";
 
-function Home() {
+export default function Home() {
+  const { sendRequest } = useFetch();
+  const [input, setInput] = useState("");
+  const [peopleList, setPeopleList] = useState([]);
+
+  const searchPeople = useCallback(
+    async (url) => {
+      const response = await sendRequest(url);
+      setPeopleList(response.results);
+    },
+    [sendRequest]
+  );
+
+  const onChange = (e) => {
+    const s = e.target.value;
+    if (s.length > 2) {
+      searchPeople(`${process.env.REACT_APP_API_URL}/people/?search=${s}`);
+    } else {
+      setPeopleList([]);
+    }
+    setInput(s);
+  };
   return (
-    <div className="home-main-div">
+    <div className="home-main">
       <h1>JS-SWAPI-REACT</h1>
+      <input
+        className="home-search-bar"
+        type="text"
+        placeholder="Search for Movie Characters"
+        onChange={onChange}
+        value={input}
+      />
+      <div className="home-list">
+        {peopleList.map((p) => (
+          <Actor key={btoa(p.url)} actor={p} />
+        ))}
+      </div>
     </div>
   );
 }
-
-export default Home;
